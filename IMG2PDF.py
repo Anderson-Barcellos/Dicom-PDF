708261f9ee7962776c1f396f748f63cf4736d556
Anders <163220534+Anderson-Barcellos@users.noreply.github.com>
2024-09-03 00:59:33 -0300

    Add files via upload

---
+++ IMG2PDF.py
@@ -0,0 +1,115 @@
+from PIL import Image
+import os
+from fpdf import FPDF
+import PyPDF2
+
+
+# Configurações da página A4
+def Make_PDF():
+    A4_WIDTH_MM = 210
+    A4_HEIGHT_MM = 297
+    MM_TO_PX = 96 / 25.4  # 96 pixels por polegada / 25.4 mm por polegada
+
+    # Configurações das imagens na tabela 2x4
+    IMAGES_PER_ROW = 2
+    ROWS = 4
+    SPACING_MM = 5  # Espaço horizontal entre as imagens em mm
+    SPACING_VERTICAL_MM = 7  # Espaço vertical entre as imagens em mm
+
+    # Calcula o tamanho disponível para cada imagem, considerando o espaço entre elas
+    available_width = A4_WIDTH_MM - ((IMAGES_PER_ROW + 1) * SPACING_MM)
+    available_height = A4_HEIGHT_MM - ((ROWS + 1) * SPACING_VERTICAL_MM)
+    image_width_mm = available_width / IMAGES_PER_ROW
+    image_height_mm = available_height / ROWS
+
+    # Cria um PDF A4
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+    # Encontra todas as imagens JPG no diretório atual
+    images = [
+        f
+        for f in os.listdir(".")
+        if f.lower().endswith((".jpg", ".jpeg", ".JPG", ".JPEG"))
+    ]
+
+    # Divide as imagens em grupos de 8
+    chunks = [images[i : i + 8] for i in range(0, len(images), 8)]
+
+    for chunk in chunks:
+        pdf.add_page()
+
+        if len(chunk) <= 6:
+            ROWS = 3
+            SPACING_VERTICAL_MM = 18
+        else:
+            ROWS = 4
+            SPACING_VERTICAL_MM = 7
+
+        # Adiciona as imagens ao PDF, mantendo a proporção
+        for i, image_path in enumerate(chunk):
+            img = Image.open(image_path)
+            img_width_mm, img_height_mm = img.size[0] / MM_TO_PX, img.size[1] / MM_TO_PX
+
+            # Mantém a proporção da imagem
+            scale_factor = min(
+                image_width_mm / img_width_mm, image_height_mm / img_height_mm
+            )
+            new_width_mm = img_width_mm * scale_factor
+            new_height_mm = img_height_mm * scale_factor
+
+            # Calcula a posição centralizada da imagem
+            x = (
+                SPACING_MM
+                + (i % IMAGES_PER_ROW) * (image_width_mm + SPACING_MM)
+                + (image_width_mm - new_width_mm) / 2
+            )
+            y = (
+                SPACING_VERTICAL_MM
+                + (i // IMAGES_PER_ROW) * (image_height_mm + SPACING_VERTICAL_MM)
+                + (image_height_mm - new_height_mm) / 4
+            )
+
+            # Adiciona a imagem ao PDF sem redimensionar o arquivo de imagem original
+            pdf.image(image_path, x=x, y=y, w=new_width_mm, h=new_height_mm)
+
+    # Salva o PDF
+    pdf.output("imagens_organizadas.pdf")
+
+
+Make_PDF()
+
+def create_percentile_plot(plots_data, output_plot):
+    weeks = np.arange(20, 40, 2)  # Semanas de gestação
+    percentil_3 = np.array([150, 250, 400, 650, 950, 1350, 1800, 2300, 2700, 3100])
+    percentil_50 = np.array([200, 350, 550, 900, 1300, 1750, 2300, 2800, 3300, 3800])
+    percentil_97 = np.array([250, 450, 700, 1150, 1600, 2200, 2700, 3200, 3700, 4200])
+
+    plt.figure(figsize=(10, 6))
+
+    for plot_name, plot_values in plots_data.items():
+        x_vals, y_vals = zip(*plot_values)
+        plt.plot(weeks[: len(x_vals)], y_vals, "ko-", label=plot_name)
+
+    plt.plot(weeks, percentil_3, "b--", label="3% Percentile")
+    plt.plot(weeks, percentil_50, "g-", label="50% Percentile")
+    plt.plot(weeks, percentil_97, "r--", label="97% Percentile")
+    plt.fill_between(
+        weeks,
+        percentil_3,
+        percentil_97,
+        color="lightgrey",
+        alpha=0.5,
+        label="3%-97% Range",
+    )
+
+    plt.xlabel("Weeks of Gestation")
+    plt.ylabel("Estimated Fetal Weight (g)")
+    plt.title("Fetal Growth Chart with Percentiles")
+    plt.legend(loc="upper left")
+    plt.grid(True)
+    plt.xlim(20, 40)
+    plt.ylim(0, 4500)
+    plt.tight_layout()
+
+    plt.savefig(output_plot)
+    plt.close()
--- main.py
+++ main.py
@@ -1,133 +1,64 @@
 from DicomManager.unzip import Unzipper
 from DicomManager.DICOM import DICOM2JPEG
-
 import os
-from PIL import Image
-from reportlab.lib.pagesizes import A4
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 from reportlab.platypus import Image as rlImage
 from reportlab.lib import colors
+from PDFMAKER.pdfmaker import MkPDF
+from PyPDF2 import PdfMerger
 
+path = "C:\\Users\\Administrador\\Desktop\\NEWDICOM\\Pacientes"
 
+def Merger(path:str):
+    print("Analisando PDFs...")
+    path = path
+    try:
+        prefixes = set()
+        pdf_pairs = []
 
-def MkPDF(name: str):
-    """Function to create a PDF file with images. The images will be added to the PDF without resizing, but their display size will be adjusted to fit the page."""
-    # Initialize PDF
-    # Margens do documento
-    doc_margin = 20
+        # Identificar os prefixos dos arquivos
+        for file in os.listdir(path):
+            if " " in file:
+                prefix = file.split(" ")[0]
+                prefixes.add(prefix)
 
-    # Initialize PDF with adjusted margins
-    pdf = SimpleDocTemplate(f"{name[15:]}.pdf", pagesize=A4, rightMargin=doc_margin, leftMargin=doc_margin, topMargin=20, bottomMargin=doc_margin)
-    
+        # Verificar se os arquivos de cada prefixo existem
+        for prefix in prefixes:
+            pdf1 = f"{prefix}.pdf"
+            pdf2 = None
+            for file in os.listdir(path):
+                if file.startswith(prefix) and " " in file:
+                    pdf2 = file
+                    break
+            if (
+                pdf2
+                and os.path.exists(os.path.join(path, pdf1))
+                and os.path.exists(os.path.join(path, pdf2))
+            ):
+                pdf_pairs.append((pdf1, pdf2))
 
+        # Realizar a fusão dos arquivos PDF
+        for pdf1, pdf2 in pdf_pairs:
+            if os.path.exists(f"Laudo_{pdf2[:-4]}.pdf"):
+                print("Nada a unir!")
+                continue
+            else:
+                output_filename = f"Laudo_{pdf2[:-4]}.pdf"
+                merger = PdfMerger()
+                merger.append(os.path.join(path, pdf1))
+                merger.append(os.path.join(path, pdf2))
+                merger.write(output_filename)
+                merger.close()
 
-
-    # Create current folder path
-    folder = os.path.join(os.getcwd(), "Images")
-
-    # Table settings
-    num_rows = 4
-    num_cols = 2
-    cell_padding = 5
-
-
-    width, height = A4
-    width -= 1 * doc_margin  # Adjust width for margins
-    height -= 1 * doc_margin  # Adjust height for margins
-    table_width = 0.98* width
-    table_height = 0.98 * height
-    cell_width = table_width / num_cols
-    cell_height = table_height / num_rows
-
-    # List all JPG images in the folder
-
-    def T(): 
-        images = [
-            f for f in os.listdir(folder) if f.endswith(".jpeg") or f.endswith(".jpg") or f.endswith(".png") or f.endswith(".JPG")
-        ]
-        data = []
-        data2 = []
-        # Start table data list
-        
-        def imager(data,images): 
-            # Add images to the data list
-            for i in range(num_rows):
-                row = []
-                for j in range(num_cols):
-                    try:
-                        img_path = images.pop(0)
-                        img_path = os.path.join(folder, img_path)
-                    except IndexError:
-                        row.append("")  # No more images to add
-                        continue
-
-            
-                    img = Image.open(img_path)
-                    rl_img = rlImage(img_path)
-
-
-                    # Adjust the display size of the image in the PDF
-                    w_ratio = (cell_width - 2 * cell_padding) / img.size[0]
-                    h_ratio = (cell_height - 2 * cell_padding) / img.size[1]
-                    ratio = min(w_ratio, h_ratio)
-
-                    rl_img.drawHeight = img.size[1] * ratio
-                    rl_img.drawWidth = img.size[0] * ratio
-
-                    row.append(rl_img)
-
-                data.append(row)
-            return data
-               # Create the table
-
-        if len(images) <=8:
-            data = imager(data,images)
-            table = Table(data)
-            table.setStyle(
-                TableStyle(
-                    [
-                        ("GRID", (0, 0), (-1, -1), 0, colors.transparent),
-                        ("VALIGN", (0, 0), (-1, -1), "MIDDLE"),
-                    ]
-                )
-            )
-            pdf.build([table])
-            print(f"PDF criado com sucesso")
-
-                  
-        else:
-            half =images[:8]
-            data = imager(data,half)
-            other_half = images[8:]
-            data2 = imager(data2,other_half)
-
-            table = Table(data)
-            table.setStyle(
-                TableStyle(
-                    [
-                        ("GRID", (0, 0), (-1, -1), 0, colors.transparent),
-                        ("VALIGN", (0, 0), (-1, -1), "MIDDLE"),
-                    ]
-                )
-            )
-
-            table2 = Table(data2)
-            table2.setStyle(
-                TableStyle(
-                    [
-                        ("GRID", (0, 0), (-1, -1), 0, colors.transparent),
-                        ("VALIGN", (0, 0), (-1, -1), "MIDDLE"),
-                    ]
-                )
-            )
-            pdf.build([table,table2])
-            print(f"PDF criado com sucesso")
-    T()
-
-
+            print("PDFs Unidos!")
+    except Exception as e:
+        print(e)
 
 
 def Extract_Convert_Img(file: str):
+    """Function to extract and convert DICOM images to JPEG format.
+    #### Parameters:
+    - file: str
+        Name of the ZIP file containing the DICOM images."""
     # Extract the file
     unzipper = Unzipper(f"{file}", "./Dicoms")
     unzipper.unzipper()
@@ -137,10 +68,28 @@ def Extract_Convert_Img(file: str):
     dicom2jpeg.converter()
     MkPDF(name)
     dicom2jpeg.eliminate_folders()
-    #Test for a any other pdf file on the current folder
+    # Test for a any other pdf file on the current folder
 
     print("Extração e conversão concluídas com sucesso!")
 
+
+def ExtractSR(file: str):
+    """Function to extract and convert DICOM images to JPEG format.
+    #### Parameters:
+    - file: str
+        Name of the ZIP file containing the DICOM images."""
+    # Extract the file
+    unzipper = Unzipper(f"{file}", "./Dicoms")
+    unzipper.unzipper()
+    name = unzipper.name
+    print(name)
+    dicom2jpeg = DICOM2JPEG("./Dicoms", "./Images")
+    dicom2jpeg.converter()
+    MkPDF(name)
+    dicom2jpeg.eliminate_folders()
+    # Test for a any other pdf file on the current folder
+
+    print("Extração e conversão concluídas com sucesso!")
 
 
 # ORTHANC
@@ -148,36 +97,57 @@ from pyorthanc import Orthanc
 import time
 import json
 
-orthanc = Orthanc("http://", "orthanc", "orthanc")
-
-while True:
-    # Load pacientes atualizados
-    patients = None
-    with open('patients.json') as json_file:
-        patients = json.load(json_file)
-
-    # Atualização os pacientes
-    latest_patients = orthanc.get_patients()
-    if patients == latest_patients:
-        print("Nenhum novo paciente encontrado")
-        time.sleep(60)
-    else:
-        new_patients = [p for p in latest_patients if p not in patients]
-
-        # Processo para cada novo paciente
-        for patient in new_patients:
-            response = orthanc.get_patients_id_archive(str(patient))
-            with open(f"ZIPS/{patient}.zip", "wb") as f:
-                f.write(response)
-            Extract_Convert_Img(f"{patient}.zip")
-
-        # Atualizar pacientes
-        patients = latest_patients
+orthanc = Orthanc("http://170.238.45.149:8042", "orthanc", "orthanc")
 
 
-        
-        with open('patients.json', 'w') as json_file:
-            json.dump(patients, json_file)
+def connection():
+    max_attempts = 5  # Número máximo de tentativas antes de abortar
+    attempt = 0
+    while True:
+        try:
+            while True:
+                # Load pacientes atualizados
+                patients = None
+                with open("patients.json") as json_file:
+                    patients = json.load(json_file)
 
-        # Esperar um momento (tempo em segundos) antes de verificar novamente
-        time.sleep(60)
+                # Atualização os pacientes
+                latest_patients = orthanc.get_patients()
+
+                if patients == latest_patients:
+                    print("Nenhum novo paciente encontrado")
+                    Merger(path)
+                    time.sleep(15)
+                else:
+                    new_patients = [p for p in latest_patients if p not in patients]
+
+                    # Processo para cada novo paciente
+                    for patient in new_patients:
+
+                        response = orthanc.get_patients_id_archive(str(patient))
+                        print(f"Baixando paciente {patient}...")
+                        with open(f"ZIPS/{patient}.zip", "wb") as f:
+                            f.write(response)
+                        Extract_Convert_Img(f"{patient}.zip")
+
+                    # Atualizar pacientes
+                    patients = latest_patients
+
+                    with open("patients.json", "w") as json_file:
+                        json.dump(patients, json_file)
+
+                    # Esperar um momento (tempo em segundos) antes de verificar novamente
+                    time.sleep(15)
+        except Exception as e:
+            attempt += 1
+            print(f"Ocorreu um erro: {e}. Tentativa {attempt} de {max_attempts}")
+            if attempt >= max_attempts:
+                print("Número máximo de tentativas atingido. Abortando.")
+
+                break
+            else:
+                connection()
+                time.sleep(5)  # Pequeno delay antes de tentar novamente
+
+
+connection()
